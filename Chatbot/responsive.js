5f88d09ffaee92a7aeef5789e487f1edc6db5467
Kankshi Shah <127223655+kankshi19@users.noreply.github.com>
2023-04-03 17:03:03 +0530

    Add files via upload

---
+++ Chatbot/responsive.js
@@ -0,0 +1,13 @@
+const responseObj = 
+{
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    Hello : "Hey ! How are you doing ?",
+    Hey : "Hey! What's Up",
+    //WhoisKankshiShah: " She is a student studying in Bhagubhai Polytechnic.She is passionate about coding and possesses Strong creative and Analytical skills.",
+    good : "That's great!!",
+    hi:"Hey! What's Up", 
+    today: new Date().toDateString(),
+
+    time: new Date().toLocaleTimeString(),
+  };
